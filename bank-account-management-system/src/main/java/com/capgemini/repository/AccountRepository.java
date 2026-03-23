c9059e4e995147bcff1e2fdc7bd2978cb424b52b
cser-arya-vajpai <cser.arya.vajpai@gmail.com>
2026-03-23 11:17:39 +0530

    Delete bank-account-management-system directory

--- bank-account-management-system/src/main/java/com/capgemini/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.repository;
-
-import com.capgemini.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-}
