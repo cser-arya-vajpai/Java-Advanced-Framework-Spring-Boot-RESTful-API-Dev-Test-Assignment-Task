7e9408f0da6a3244fbb90e4148032ad815fe8461
cser-arya-vajpai <cser.arya.vajpai@gmail.com>
2026-03-23 11:18:06 +0530

    Delete online-food-order-management-system directory

--- online-food-order-management-system/src/main/java/com/capgemini/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.repository;
-
-import com.capgemini.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
